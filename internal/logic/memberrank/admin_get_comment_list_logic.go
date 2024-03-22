5b279ef3f3ce26b5c881dc9f883821c04afabffb
ZhiNen <a1679415186@163.com>
2024-03-22 17:02:22 +0800

    获取用户留言列表添加用户信息

--- internal/logic/memberrank/admin_get_comment_list_logic.go
+++ internal/logic/memberrank/admin_get_comment_list_logic.go
@@ -70,10 +70,10 @@ func (l *AdminGetCommentListLogic) AdminGetCommentList(req *types.CommentListReq
 			Update_time: info.GetUpdateTime(),
 			IsReply:     info.GetIsReply(),
 			Reply:       respVos,
-			UserName:    "",
-			NickName:    "",
-			Email:       "",
-			Avatar:      "",
+			UserName:    info.GetUserName(),
+			NickName:    info.GetNickName(),
+			Email:       info.GetEmail(),
+			Avatar:      info.GetAvatar(),
 		})
 	}
 
